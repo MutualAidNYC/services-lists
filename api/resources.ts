d02869caa98021b2a26232ac2f292ac3645504d0
dangalan <alanhuydang@gmail.com>
2021-04-28 20:42:59 -0400

    Remove hard-coded Airtable key used for manual testing

--- api/resources.ts
+++ api/resources.ts
@@ -2,8 +2,7 @@ import { AirtableClient } from './airtable'
 import { Service } from '../models'
 import { keys } from 'ts-transformer-keys'
 
-//const resourcesClient = new AirtableClient(process.env.RESOURCES_API_KEY, process.env.RESOURCES_BASE_ID)
-const resourcesClient = new AirtableClient('keyxHldVjXyylE5zc', 'appivaPwNjH0Rs2RT')
+const resourcesClient = new AirtableClient(process.env.RESOURCES_API_KEY, process.env.RESOURCES_BASE_ID)
 
 export const getServices = (filter?: string): Promise<Service[]> => {
   return resourcesClient.get<Service>('services', keys<Service>(), filter)
