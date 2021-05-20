2b703597936495d4494603fc34c4380bd9aee6c2
dangalan <alanhuydang@gmail.com>
2021-05-19 21:58:44 -0400

    Airtable API calls to get services lists and services (#1)
    * Added function to get services from resources Airtable
    
    * Remove hard-coded Airtable key used for manual testing
    
    * Add api call for services lists, switch services api call to get record by id

---
+++ api/airtable.ts
@@ -0,0 +1,59 @@
+import Airtable from 'airtable'
+import Base from 'airtable/lib/base';
+import Record from 'airtable/lib/record';
+import Table from 'airtable/lib/table';
+
+export class AirtableClient {
+  base: {
+    (tableName: string): Table;
+    _base: Base;
+    getId(): string;
+    makeRequest(options: {
+        method?: string;
+        path?: string;
+        qs?: globalThis.Record<string, any>;
+        headers?: globalThis.Record<string, any>;
+        body?: globalThis.Record<string, any>;
+        _numAttempts?: number;
+    }): Promise<Response & {
+        statusCode: number;
+    }>;
+    table(tableName: string): Table;
+  }
+
+  constructor(apiKey: string, baseId: string) {
+    this.base = new Airtable({apiKey: apiKey}).base(baseId)
+  }
+
+  async getById<O extends object>(tableName: string, keys: (keyof O)[], id: string): Promise<O> {
+    const record = await this.base.table(tableName).find(id)
+
+    const object = {} as O
+    keys.forEach(key => {
+      object[key.toString()] = record.get(key.toString())
+    })
+    return object
+  }
+
+  async get<O extends object>(tableName: string, keys: (keyof O)[],  filter?: string): Promise<O[]> {
+    let objects: O[] = []
+
+    await this.base.table(tableName).select({
+      filterByFormula: filter ? filter : ''
+    }).eachPage(
+      (records: Record[], fetchNextPage: () => void) => {
+        records.forEach(record => {
+          const object = {} as O
+          keys.forEach(key => {
+            object[key.toString()] = record.get(key.toString())
+          })
+          objects.push(object)
+        })
+
+        fetchNextPage()
+      }
+    )
+
+    return objects
+  }
+}
--- api/resources.ts
+++ api/resources.ts
@@ -1,3 +1,16 @@
-import Airtable from 'airtable'
+import { AirtableClient } from './airtable'
+import { 
+  Service,
+  ServicesLists,
+ } from '../models'
+import { keys } from 'ts-transformer-keys'
 
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID)
+const resourcesClient = new AirtableClient(process.env.RESOURCES_API_KEY, process.env.RESOURCES_BASE_ID)
+
+export const getServiceById = (id: string): Promise<Service> => {
+  return resourcesClient.getById<Service>('services', keys<Service>(), id)
+}
+
+export const getServicesLists = (filter?: string): Promise<ServicesLists[]> => {
+  return resourcesClient.get<ServicesLists>('Services Lists', keys<ServicesLists>(), filter)
+}
