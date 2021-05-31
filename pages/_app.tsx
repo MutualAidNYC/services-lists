4e13f5067000feb355283908d2ccbe75f3d6517b
dangalan <alanhuydang@gmail.com>
2021-05-31 10:14:58 -0400

    Table Component (#3)
    * Added function to get services from resources Airtable
    
    * Remove hard-coded Airtable key used for manual testing
    
    * Add api call for services lists, switch services api call to get record by id
    
    * Create Table component
    
    * Fix npm depedencies, update README

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react"
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }): JSX.Element {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
