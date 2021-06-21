bed1e7cac7a1dc4138421e815fb4b77ddd4481cb
dangalan <alanhuydang@gmail.com>
2021-06-21 17:15:48 -0400

    Display basic services lists table on homepage (#5)
    * Added function to get services from resources Airtable
    
    * Remove hard-coded Airtable key used for manual testing
    
    * Add api call for services lists, switch services api call to get record by id
    
    * Create Table component
    
    * Fix npm depedencies, update README
    
    * Render basic table on homepage
    
    * Delete unused variable

--- api/resources.ts
+++ api/resources.ts
@@ -1,16 +1,16 @@
 import { AirtableClient } from './airtable'
 import { 
   Service,
-  ServicesLists,
+  ServicesList,
  } from '../models'
 import { keys } from 'ts-transformer-keys'
 
-const resourcesClient = new AirtableClient(process.env.RESOURCES_API_KEY, process.env.RESOURCES_BASE_ID)
+const resourcesClient = new AirtableClient(process.env.NEXT_PUBLIC_RESOURCES_API_KEY, process.env.NEXT_PUBLIC_RESOURCES_BASE_ID)
 
 export const getServiceById = (id: string): Promise<Service> => {
   return resourcesClient.getById<Service>('services', keys<Service>(), id)
 }
 
-export const getServicesLists = (filter?: string): Promise<ServicesLists[]> => {
-  return resourcesClient.get<ServicesLists>('Services Lists', keys<ServicesLists>(), filter)
+export const getServicesLists = (filter?: string): Promise<ServicesList[]> => {
+  return resourcesClient.get<ServicesList>('Services Lists', keys<ServicesList>(), filter)
 }
