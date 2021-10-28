610bf6b14ce3150e90b14f273d836742f24ac6c5
dangalan <alanhuydang@gmail.com>
2021-10-28 18:19:01 -0400

    Add filtering by taxonomies (#12)
    * Add filtering by taxonomies
    
    * Add services lists to useEffect dependencies
    
    * Reset data without filters, remove console.log
    
    * Remove more console logs

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,2 +1,1 @@
-export * from './useSearch'
 export * from './useServicesLists'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   Heading,
+  HStack,
   Menu,
   MenuButton,
   MenuDivider,
@@ -19,38 +20,54 @@ export default function Home(): JSX.Element {
     sortFieldsTextToVal,
     setOrder,
     setSortFields,
+    taxonomies,
+    setFilters,
   } = servicesListHandler
 
   return (
     <ServicesListsProvider value={servicesListHandler}>
       <Heading fontSize='heading1' mb='36px'>Services Lists</Heading>
       <SearchBar handleSearch={handleSearch} w='66%' mb='24px' />
-      <Menu closeOnSelect={false}>
-        <MenuButton as={Button} mb='24px'>Sort by</MenuButton>
-        <MenuList>
-          <MenuOptionGroup
-            title="Order"
-            type="radio"
-            defaultValue={order}
-            onChange={e => setOrder(e)}
-          >
-            <MenuItemOption value="asc">Ascending</MenuItemOption>
-            <MenuItemOption value="desc">Descending</MenuItemOption>
-          </MenuOptionGroup>
-          <MenuDivider />
-          <MenuOptionGroup
-            title="Sort by"
-            type="checkbox"
-            onChange={e => setSortFields(e)}
-          >
-            {Object.keys(sortFieldsTextToVal).map((text, i) =>
-              <MenuItemOption value={sortFieldsTextToVal[text]} key={i}>
-                {text}
-              </MenuItemOption>
-            )}
-          </MenuOptionGroup>
-        </MenuList>
-      </Menu>
+      <HStack spacing="24px" mb="24px">
+        <Menu closeOnSelect={false}>
+          <MenuButton as={Button}>Sort by</MenuButton>
+          <MenuList>
+            <MenuOptionGroup
+              title="Order"
+              type="radio"
+              defaultValue={order}
+              onChange={e => setOrder(e)}
+            >
+              <MenuItemOption value="asc">Ascending</MenuItemOption>
+              <MenuItemOption value="desc">Descending</MenuItemOption>
+            </MenuOptionGroup>
+            <MenuDivider />
+            <MenuOptionGroup
+              title="Sort by"
+              type="checkbox"
+              onChange={e => setSortFields(e)}
+            >
+              {Object.keys(sortFieldsTextToVal).map((text, i) =>
+                <MenuItemOption value={sortFieldsTextToVal[text]} key={i}>
+                  {text}
+                </MenuItemOption>
+              )}
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+        <Menu closeOnSelect={false}>
+          <MenuButton as={Button}>Filter by</MenuButton>
+          <MenuList>
+            <MenuOptionGroup type="checkbox" onChange={e => setFilters(e)}>
+              {taxonomies?.map((taxonomy, i) => 
+                <MenuItemOption value={taxonomy} key={i}>
+                  {taxonomy}
+                </MenuItemOption>
+              )}
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </HStack>
       <ServicesLists />
     </ServicesListsProvider>
   )
