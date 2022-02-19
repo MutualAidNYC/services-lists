59f64f891a1299591dda1fd5d52c453fa50e8a39
Ben Moore <benmoore@MacBook-Pro.local>
2022-02-19 16:59:32 -0500

    Addingn new const + calling function

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,6 +1,8 @@
 import { getAllServices } from 'api'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const SERVICES_CACHE_TIMEOUT = 60 * 1000 // 60 Seconds
+
 // Caching to circumvent Airtable API Limits
 var services_caches_time: number
 var services_caches_data: any
@@ -12,7 +14,7 @@ export default async (_: NextApiRequest, res: NextApiResponse) => {
 
 async function getAllServicesCached() { 
   const curr_time =  Date.now()
-  if (services_caches_time == null || services_caches_data == null || curr_time - (60*1000) > services_caches_time) {
+  if (services_caches_time == null || services_caches_data == null || curr_time - SERVICES_CACHE_TIMEOUT > services_caches_time) {
     const services = await getAllServices()
     services_caches_time = curr_time
     services_caches_data = services
--- pages/api/taxonomies.ts
+++ pages/api/taxonomies.ts
@@ -1,18 +1,20 @@
 import { getAllTaxonomies } from 'api'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const TAXONOMIES_CACHE_TIMEOUT = 60 * 1000 // 60 Seconds
+
 // Caching to circumvent Airtable API Limits
 var taxonomies_caches_time: number
 var taxonomies_caches_data: any
 
 export default async (_: NextApiRequest, res: NextApiResponse) => {
-  const taxonomies = await getAllTaxonomies()
+  const taxonomies = getAllTaxonomiesCached()
   res.status(200).json(taxonomies)
 }
 
 async function getAllTaxonomiesCached() { 
   const curr_time =  Date.now()
-  if (taxonomies_caches_time == null || taxonomies_caches_data == null || curr_time - (60*1000) > taxonomies_caches_time) {
+  if (taxonomies_caches_time == null || taxonomies_caches_data == null || curr_time - TAXONOMIES_CACHE_TIMEOUT > taxonomies_caches_time) {
     const taxonomies = await getAllTaxonomies()
     taxonomies_caches_time = curr_time
     taxonomies_caches_data = taxonomies
