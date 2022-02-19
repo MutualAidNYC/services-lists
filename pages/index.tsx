f0876f871015de7474823cc9458e7c5c5a16c161
Veronica Li <veronica.li@verizonmedia.com>
2022-02-19 17:50:02 -0500

    added nazmus's styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,73 +1,83 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
-import { Heading, HStack, Stack, Box, VStack, Text } from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import {
+  Heading,
+  HStack,
+  Stack,
+  Box,
+  VStack,
+  Text,
+  Spacer,
+} from '@chakra-ui/react'
 import { SearchBar, ServicesListItem, SortMenu } from 'components'
 import { useAllServicesLists, useTaxonomyFilter } from 'hooks'
 import { ServicesList } from 'models'
 import { NextPage } from 'next'
 import { useCallback, useEffect, useState } from 'react'
 import Select from 'react-select'
+import { PaginatedList } from 'react-paginated-list'
 
-export const HomePage: NextPage = () => {
-  const { baseServicesLists, servicesLists, setServicesLists } =
-    useAllServicesLists()
-
-  const filterByTaxonomies = useCallback(
-    (servicesList: ServicesList, filters: string[]) =>
-      servicesList.taxonomies?.some((taxonomy) => filters.includes(taxonomy)) ??
-      false,
-    []
-  )
-  const { taxonomyOptions, setFilters } = useTaxonomyFilter(
+export const Home: NextPage = () => {
+  const {
     baseServicesLists,
+    servicesLists,
     setServicesLists,
     filterByTaxonomies
-  )
+  } = useAllServicesLists()
+
+  const filterFunction = (servicesList: ServicesList, filters: string[]) =>
+    servicesList.taxonomies?.some(taxonomy => filters.includes(taxonomy)) ?? false
+  const { taxonomyOptions, setFilters } = useTaxonomyFilter(baseServicesLists, setServicesLists, filterFunction)
 
   const sortFieldsTextToVal = { Name: 'name', Description: 'description' }
 
-  const [displayAmount, setDisplayAmount] = useState([0, 9])
+  const [maxAmountDisplayed, setMaxAmountDisplayed] = useState(10)
 
-  useEffect(() => {
-    if (servicesLists?.length < 70) {
-      setDisplayAmount([0, servicesLists.length])
-    }
-  }, [])
+
+  const displayAmountOptions = [
+    { value: 5, label: '5' },
+    { value: 10, label: '10' },
+    { value: 25, label: '25' },
+    { value: 50, label: '50' },
+    { value: 100, label: '100' },
+    { value: servicesLists.length, label: 'All' }
+  ]
+
+
 
   return (
-    <Box w="100%" mb={8} display="flex" flexDirection="column">
-      <VStack
-        w="100%"
-        mb={8}
-        alignItems="left"
-        pl={{ base: 4, sm: 16, md: 32 }}
-      >
-        <Heading fontSize={{ base: '4xl', sm: '5xl' }} mt={8}>
-          Services Lists
-        </Heading>
-        <SearchBar
-          baseData={baseServicesLists}
-          setData={setServicesLists}
-          searchFields={['name', 'description']}
-          w={{ base: '100%', sm: '60%' }}
-          mb="24px"
-        />
-        <HStack spacing="24px" w="100%" mb="24px" mt={2}>
-          <SortMenu
-            data={servicesLists}
-            setData={setServicesLists}
-            sortFieldsTextToVal={sortFieldsTextToVal}
-          />
+    <Box w='100%' mb={8} display='flex' flexDirection='column'>
+      <VStack w='100%' mb={8} alignItems='left' pl={{ base: 4, sm: 16, md: 32 }} >
+        <Heading fontSize={{ base: '4xl', sm: '5xl' }} mt={8} mb={4}>Services Lists</Heading>
 
-          <Box>
+        <Stack w='100%' direction={'row'} justifyContent='space-between' >
+          <SearchBar baseData={baseServicesLists} setData={setServicesLists} searchFields={['name', 'description']} w={{ base: '100%', sm: '60%' }} mb='24px' />
+          <Box display='flex' flex-flexDirection='row' justifyContent='center' alignItems='center' pr={16}>
+            <Text textAlign='center' px={2} > Results per page</Text>
+            <Select
+              isSearchable
+              closeMenuOnSelect={true}
+              placeholder={`${maxAmountDisplayed}`}
+              options={displayAmountOptions}
+              onChange={(e) => { e ? setMaxAmountDisplayed(e.value) : null }}
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 16,
+              })}
+            />
+          </Box>
+        </Stack>
+
+        <HStack spacing="24px" w='100%' mb="24px" mt={2} >
+          <SortMenu data={servicesLists} setData={setServicesLists} sortFieldsTextToVal={sortFieldsTextToVal} />
+
+          <Box >
             <Select
               isMulti
               isSearchable
               closeMenuOnSelect={false}
               placeholder="Filter By"
               options={taxonomyOptions}
-              onChange={(e) => {
-                setFilters(e.map((e) => e.value))
-              }}
+              onChange={e => { setFilters(e.map((e) => e.value)) }}
               theme={(theme) => ({
                 ...theme,
                 borderRadius: 16,
@@ -79,80 +89,46 @@ export const HomePage: NextPage = () => {
               })}
             />
           </Box>
+
         </HStack>
       </VStack>
-      <Stack spacing="36px" alignItems="left" pl={{ base: 4, sm: 16, md: 32 }}>
-        {servicesLists?.map((servicesList, i) => (
-          <Box
-            key={i}
-            display={
-              i >= displayAmount[0] && i <= displayAmount[1]
-                ? 'inherit'
-                : 'none'
-            }
-          >
-            <ServicesListItem
-              pr={{ base: '2px', sm: '8px' }}
-              w="100%"
-              _hover={{
-                base: {},
-                md: { background: '#fafafa', borderRadius: '24px' },
-              }}
-              key={servicesList.name}
-              servicesList={servicesList}
-            />
-          </Box>
-        ))}
-      </Stack>
 
-      <VStack
-        w="100%"
-        mt={8}
-        px={{ base: '4px', sm: '8px', md: '16px' }}
-        spacing={0}
-      >
-        <Stack w="100%" justifyContent="space-between" direction="row">
-          <Box />
-          <Text fontWeight="bold">
-            {' '}
-            {displayAmount[1] + 1 - displayAmount[0] + '  results loaded.'}{' '}
-          </Text>
-          <Text fontSize="md" pr={{ base: '12px' }}>
-            {' '}
-            {displayAmount[0] + 1 + ' ... ' + (displayAmount[1] + 1)}{' '}
-          </Text>
-        </Stack>
 
-        <HStack alignItems="left" justifyContent="right" w="100%">
-          <ChevronLeftIcon
-            w="32px"
-            h="32px"
-            _hover={{ width: '40px', height: '40px' }}
-            onClick={() => {
-              displayAmount[0] - 10 >= 0
-                ? setDisplayAmount([
-                    displayAmount[0] - 10,
-                    displayAmount[1] - 10,
-                  ])
-                : null
-            }}
-          />
+      <Box>
+        <PaginatedList
+          list={servicesLists}
+          useMinimalControls={true}
+          itemsPerPage={maxAmountDisplayed}
+          renderList={(list) => (
+            <>
+              {
+                list.map((item, i) => {
+                  return (
+                    <Stack alignItems='left' key={i} w='100%' maxW='4xl' pl={{ base: 4, sm: 16, md: 32 }} >
+                      <ServicesListItem
+                        pr={{ base: '2px', sm: '8px' }}
+                        w='100%'
+                        _hover={{ base: {}, md: { background: '#fafafa', borderRadius: '24px' } }}
+                        key={item.name}
+                        servicesList={item}
+                      />
+                    </Stack>
+                  )
+                })
+              }
+              <Text textAlign='center' fontWeight='light' > {list.length == 1 ? ` Showing ${list.length} out of ${servicesLists.length} results.` : ` Showing ${list.length} out of ${servicesLists.length} results.`}  </Text>
+              {/* <HStack justifyContent='center' w='100%' cursor='pointer' onClick={() => {maxAmountDisplayed + maxAmountDisplayed > servicesLists.length? setMaxAmountDisplayed(servicesLists.length) : setMaxAmountDisplayed(maxAmountDisplayed + 1 )}}>
+                <Text fontWeight='bold'> Load more</Text>
+                <ChevronDownIcon />
+              </HStack> */}
+            </>
+          )}
+        />
+      </Box>
 
-          <ChevronRightIcon
-            w="32px"
-            h="32px"
-            _hover={{ width: '40px', height: '40px' }}
-            onClick={() => {
-              displayAmount[1] + 10 <= servicesLists?.length
-                ? setDisplayAmount([
-                    displayAmount[0] + 10,
-                    displayAmount[1] + 10,
-                  ])
-                : null
-            }}
-          />
-        </HStack>
-      </VStack>
     </Box>
+
   )
 }
+
+export default Home;
