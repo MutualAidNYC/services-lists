c5c1d0034039e86396bc49bae86b015f917b76d1
Ben Moore <btmoore40@gmail.com>
2022-02-19 17:05:53 -0500

    Adding Dumb Cacheing Logic for API Calls (#29)
    * Adding Dumb Cacheing Logic for API Calls
    
    * Addingn new const + calling function
    
    Co-authored-by: Ben Moore <benmoore@MacBook-Pro.local>

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,7 +1,27 @@
 import { getAllServices } from 'api'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const SERVICES_CACHE_TIMEOUT = 60 * 1000 // 60 Seconds
+
+// Caching to circumvent Airtable API Limits
+var services_caches_time: number
+var services_caches_data: any
+
 export default async (_: NextApiRequest, res: NextApiResponse) => {
-  const services = await getAllServices()
+  const services = getAllServicesCached()
   res.status(200).json(services)
+}
+
+async function getAllServicesCached() { 
+  const curr_time =  Date.now()
+  if (services_caches_time == null || services_caches_data == null || curr_time - SERVICES_CACHE_TIMEOUT > services_caches_time) {
+    const services = await getAllServices()
+    services_caches_time = curr_time
+    services_caches_data = services
+    console.log('Retreiving Services from Airtable')
+    return services
+  } else {
+    console.log('Retreiving Cached Services')
+    return services_caches_data
+  }
 }
