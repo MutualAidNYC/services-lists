c02b984e7d627cee61be30e08270db75b1ba1c14
Ben Moore <benmoore@MacBook-Pro.local>
2022-02-19 16:49:43 -0500

    Adding Dumb Cacheing Logic for API Calls

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,7 +1,25 @@
 import { getAllServices } from 'api'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+// Caching to circumvent Airtable API Limits
+var services_caches_time: number
+var services_caches_data: any
+
 export default async (_: NextApiRequest, res: NextApiResponse) => {
-  const services = await getAllServices()
+  const services = getAllServicesCached()
   res.status(200).json(services)
+}
+
+async function getAllServicesCached() { 
+  const curr_time =  Date.now()
+  if (services_caches_time == null || services_caches_data == null || curr_time - (60*1000) > services_caches_time) {
+    const services = await getAllServices()
+    services_caches_time = curr_time
+    services_caches_data = services
+    console.log('Retreiving Services from Airtable')
+    return services
+  } else {
+    console.log('Retreiving Cached Services')
+    return services_caches_data
+  }
 }
--- pages/api/taxonomies.ts
+++ pages/api/taxonomies.ts
@@ -1,7 +1,25 @@
 import { getAllTaxonomies } from 'api'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+// Caching to circumvent Airtable API Limits
+var taxonomies_caches_time: number
+var taxonomies_caches_data: any
+
 export default async (_: NextApiRequest, res: NextApiResponse) => {
   const taxonomies = await getAllTaxonomies()
   res.status(200).json(taxonomies)
+}
+
+async function getAllTaxonomiesCached() { 
+  const curr_time =  Date.now()
+  if (taxonomies_caches_time == null || taxonomies_caches_data == null || curr_time - (60*1000) > taxonomies_caches_time) {
+    const taxonomies = await getAllTaxonomies()
+    taxonomies_caches_time = curr_time
+    taxonomies_caches_data = taxonomies
+    console.log('Retreiving Taxonomies from Airtable')
+    return taxonomies
+  } else {
+    console.log('Retreiving Cached Taxonomies')
+    return taxonomies_caches_data
+  }
 }
