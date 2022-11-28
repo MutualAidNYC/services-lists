fab751222d3f1e550109d2f53ef7f3dfd765e60a
notnazsty <nazmus6685@gmail.com>
2022-11-28 12:27:03 -0500

    pr fixes + password/email auth

--- models/users.ts
+++ models/users.ts
@@ -1,11 +1,11 @@
 export interface UserDoc {
-    name: string,
-    email: string,
-    id: string,
-    lists: string[] //Array of list ids
+  name: string
+  email: string
+  id: string
+  lists: string[] //Array of list ids
 }
 
 export interface PasswordAuthResponse {
-    code: number;
-    message: string;
+  code: number
+  message: string
 }
--- utils/firebase/userData.ts
+++ utils/firebase/userData.ts
@@ -1,16 +1,19 @@
-import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
-import { UserDoc } from "models/users";
-import { userRef } from "./init";
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import { UserDoc } from 'models/users'
+import { userRef } from './init'
 
-export const addToUserOwnedLists = async (listID: string, userData: UserDoc) => {
-    await updateDoc(doc(userRef, userData.id), {
-        listID: arrayUnion(listID)
-    })
+export const addToUserOwnedLists = async (
+  listID: string,
+  userData: UserDoc
+) => {
+  await updateDoc(doc(userRef, userData.id), {
+    listID: arrayUnion(listID),
+  })
 }
 
 //Should be used when deleting a list
 export const removeListFromUser = async (listID: string, userData: UserDoc) => {
-    await updateDoc(doc(userRef, userData.id), {
-        listID: arrayRemove(listID)
-    })
+  await updateDoc(doc(userRef, userData.id), {
+    listID: arrayRemove(listID),
+  })
 }
