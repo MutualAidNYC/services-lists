0b36ae67fa3fe6515a10b7e5450f47cab36c5fb8
dangalan <alanhuydang@gmail.com>
2023-11-15 19:40:41 -0500

    Even more WIP reorganizing of DB code

--- pages/api/services-lists/[id].ts
+++ pages/api/services-lists/[id].ts
@@ -1,7 +1,19 @@
-import { findServiceList } from 'apiFunctions'
+import { ResourcesAirtableClient } from 'database'
+import { Collection } from 'models'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const serviceList = await findServiceList(req.query.id as string)
-  res.status(200).json(serviceList)
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<Collection>
+) => {
+  res
+    .status(200)
+    .json(
+      (
+        await ResourcesAirtableClient.find<Collection>(
+          'Resource Lists',
+          req.query.id as string
+        )
+      ).fields
+    )
 }
--- pages/api/services-lists/index.ts
+++ pages/api/services-lists/index.ts
@@ -1,18 +1,32 @@
-import { createServicesLists, selectAllServicesLists } from 'apiFunctions'
-import { CreateServicesListRequest } from 'models'
+import { QueryParams } from 'airtable/lib/query_params'
+import { createServicesLists } from 'apiFunctions'
+import { ResourcesAirtableClient } from 'database'
+import { Collection, CreateServicesListRequest } from 'models'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Cache } from 'utils'
 import { addToUserOwnedLists } from 'utils/firebase'
 
-const cache = new Cache(selectAllServicesLists, 'GET /api/services-lists')
+const selectAllCollections = async (
+  queryParams?: QueryParams<Collection>
+): Promise<Collection[]> => {
+  return (
+    await ResourcesAirtableClient.selectAll<Collection>(
+      'Resource Lists',
+      queryParams
+    )
+  ).map((record) => record.fields)
+}
+
+const cache = new Cache(selectAllCollections, 'GET /api/services-lists')
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'GET': {
-      const servicesLists = await cache.getCachedData(
-        req.query.filter as string
+      res.status(200).json(
+        await cache.getCachedData({
+          filterByFormula: req.query.filter as string,
+        })
       )
-      res.status(200).json(servicesLists)
       break
     }
     case 'POST': {
